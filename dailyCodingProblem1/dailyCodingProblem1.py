9610d243032f72cb5d1cd834d93efdd2358ffd1d
Alane Marie de Lima <alane93@gmail.com>
2023-12-15 23:07:47 -0300

    Update dailyCodingProblem1.py

--- dailyCodingProblem1/dailyCodingProblem1.py
+++ dailyCodingProblem1/dailyCodingProblem1.py
@@ -1,45 +1,45 @@
-#Problem from Google:
+'''Good morning! Here's your coding interview problem for today.
 
-#Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-#For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-#Bonus: Can you do this in one pass?
+This problem was recently asked by Google.
 
-######
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-# - input: list of integers l and integer value k
-# - output: True if l has two numbers a and b where a+b=k and False otherwise
-# 
-# Complexity: Expected O(n)
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-def findSum(l, k):
-    hash = {}
+Bonus: Can you do this in one pass?'''
+
+
+def checkPair(values, k):
+    # this variable is useful to check if the numbers in the pair are the same
     half = 0
 
-    #time complexity: O(n)
-    for x in l:
-        hash[x] = k-x 
-        if(float(k-x) == k/2):
+    for i in range(len(values)):
+        v = (values[i] - k)
+        if v < 0:
+            v *= -1
+
+        if v == k/2:
             half += 1
+        else:
+            half += 2
 
-    #this loop checks if the hash has some value that lies inside l
-    #search operation in hash is expected O(1)
-    
-    #time complexity: expected O(n)
-    for key in hash:
-        if hash.get(key) in hash:
-            if((float(hash[key]) == k/2 and half >= 2) or hash[key] != k/2):
-                print(str(key)+ "+" +str(hash.get(key)))
-                return True
-
+        if v in values and half > 1:
+            return True
     return False
 
-######
 
 
 def main():
-    l = [10,15,3,7]
-    k = 15
-    print(findSum(l,k))
-    
-if __name__ == "__main__":
-    main()
+    values = [10,15,3,7]
+    k = 17
+
+    result = checkPair(values, k)
+    print(result)
+
+
+
+if __name__ == '__main__':
+   main()
+
+
+            
